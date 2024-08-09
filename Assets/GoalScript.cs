a5999cff43f1020351f75e8b6d4da343a5bd2c4a
Udagawakouta <115062885+Udagawakouta@users.noreply.github.com>
2024-08-09 12:05:13 +0900

    スペースの追加

--- Assets/GoalScript.cs
+++ Assets/GoalScript.cs
@@ -14,7 +14,7 @@ public class GoalScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isGameClear = false;
+        isGameClear = false; 
     }
 
     // Update is called once per frame
